96ccd153ee25b65b7f1dc40da6d94f26d75fe0cb
sufei <sufei@enaea.edu.cn>
2020-05-26 14:17:47 +0800

    add log

--- app/src/main/java/com/acorn/independentwebview/service/MyService.kt
+++ app/src/main/java/com/acorn/independentwebview/service/MyService.kt
@@ -6,6 +6,8 @@ import android.os.IBinder
 import android.util.Log
 import com.acorn.independentwebview.Book
 import com.acorn.independentwebview.IMyAidlInterface
+import com.acorn.independentwebview.utils.isLocalAppProcess
+import com.acorn.independentwebview.utils.logI
 
 class MyService : Service() {
     private val TAG = "MyService"
@@ -46,6 +48,7 @@ class MyService : Service() {
         }
 
         override fun addBookInOut(book: Book?) {
+            logI("service 是否是主进程:${isLocalAppProcess(baseContext)}")
             book?.let {
                 it.name = "服务器改了名字 ${it.name}"
                 bookList.add(it)
@@ -56,6 +59,7 @@ class MyService : Service() {
          * In 类型的表现形式是：数据只能由客户端传向服务端，服务端对数据的修改不会影响到客户端
          */
         override fun addBookIn(book: Book?) {
+            logI("service 是否是主进程:${isLocalAppProcess(baseContext)}")
             book?.let {
                 it.name = "我服务器改不了?!"
                 bookList.add(it)
@@ -67,6 +71,7 @@ class MyService : Service() {
          * 该对象中的属性值也是为空的，即不包含任何数据，服务端获取到该对象后，对该对象的任何操作，就会同步到客户端这边
          */
         override fun addBookOut(book: Book?) {
+            logI("service 是否是主进程:${isLocalAppProcess(baseContext)}")
             book?.let {
                 it.name = "你客户端改不了 ${it.name}"
                 bookList.add(it)
